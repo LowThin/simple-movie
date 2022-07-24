01a7ee854e8c1df54db87f44c6d16a5f59d4291e
LowThin <nguyenvanthin522000@gmail.com>
2022-07-24 16:47:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/scss";
 import Banner2 from "./components/banner/Banner2";
 import Main from "./components/layout/Main";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0anmde7SqinCLgatvInhBQIZQjLMZMWM",
+  authDomain: "movie-app-f10bc.firebaseapp.com",
+  projectId: "movie-app-f10bc",
+  storageBucket: "movie-app-f10bc.appspot.com",
+  messagingSenderId: "1048474476239",
+  appId: "1:1048474476239:web:31721b8826e52f907de1ba",
+  measurementId: "G-SZ5310LNKL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // import HomePage from "./pages/HomePage";
 // import MovieDetailPage from "./pages/MovieDetailPage";
